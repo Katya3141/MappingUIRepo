1561dfeac7b2f30987575d7b31824e8161865f9f
SpyGuyIan <spyguyian@gmail.com>
2016-10-08 14:58:20 -0400

    working on doPath

--- MappingUI/src/UI/Frames/DoPath.java
+++ MappingUI/src/UI/Frames/DoPath.java
@@ -5,18 +5,35 @@ import java.awt.Color;
 import javax.swing.JPanel;
 
 import UI.Main.MainClass;
+import UI.Util.Picture;
+import UI.Util.Pixel;
 
 public class DoPath extends JPanel{
 
 	MainClass mainClass;
-	
+	Picture thePicture;
+
 	public DoPath(MainClass myClass){
 		mainClass = myClass;//TODO resizing window reset myFrame
-		
-		//mainClass.selectionMenu.theImage
-		
-		
+		thePicture = new Picture(mainClass.selectionMenu.theImage.getPath());
+		thePicture.grayscale();
+
+
+
 		setLayout(null);
 		setBackground(Color.DARK_GRAY);
 	}
+
+	int[][] pictureToArray(){
+		Pixel[][] array = thePicture.getPixels2D();
+		int[][] intArray = new int[array.length][array[0].length];
+
+		for(Pixel[] pixArr : array){
+			for(Pixel pix : pixArr){
+				///
+			}
+		}
+		return ;
+	}
+
 }
