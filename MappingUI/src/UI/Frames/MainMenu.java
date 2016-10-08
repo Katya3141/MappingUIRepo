f83729e0a71871121efa20dcaf99397c9ccefd79
katyabulovic <katyabulovic@10.105.246.73>
2016-10-08 11:29:14 -0400

    stuff test

--- MappingUI/src/UI/Frames/MainMenu.java
+++ MappingUI/src/UI/Frames/MainMenu.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MainMenu extends JPanel{
@@ -20,9 +21,12 @@ public class MainMenu extends JPanel{
 		setBackground(Color.DARK_GRAY);
 		
 		JButton inputNewButton = new JButton("Input New Map");
-		inputNewButton.setBounds(myFrame.getWidth()/2-200, myFrame.getHeight()/2-25, 400, 50);
+		inputNewButton.setBounds(myFrame.getWidth()/2-200, 300, 400, 50);
 		JButton viewPathButton = new JButton("View Path");
-		viewPathButton.setBounds(myFrame.getWidth()/2-200, myFrame.getHeight()/2+40, 400, 50);
+		viewPathButton.setBounds(myFrame.getWidth()/2-200, 350, 400, 50);
+		
+		JLabel title = new JLabel("Mapper");
+		title.setLocation(400, 50);
 		
 		add(inputNewButton);
 		add(viewPathButton);
