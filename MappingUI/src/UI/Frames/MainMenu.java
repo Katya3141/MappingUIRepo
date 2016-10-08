5161574f3a91edc8d3f8b147dc9a734417a14ccc
SpyGuyIan <spyguyian@gmail.com>
2016-10-08 11:24:25 -0400

    MainMenu takes MainClass

--- MappingUI/src/UI/Frames/MainMenu.java
+++ MappingUI/src/UI/Frames/MainMenu.java
@@ -6,23 +6,26 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import UI.Main.MainClass;
+
 public class MainMenu extends JPanel{
 
 	private static final long serialVersionUID = 6203946356665308224L;
 	
-	private JFrame myFrame;
+	private MainClass mainClass;
 	
-	public MainMenu(JFrame frame) {
+	public MainMenu(MainClass myClass) {
 		
-		myFrame = frame; //TODO resizing window reset myFrame
+		mainClass = myClass;//TODO resizing window reset myFrame
 		
 		setLayout(null);
 		setBackground(Color.DARK_GRAY);
 		
 		JButton inputNewButton = new JButton("Input New Map");
-		inputNewButton.setBounds(myFrame.getWidth()/2-200, myFrame.getHeight()/2-25, 400, 50);
+		inputNewButton.setBounds(mainClass.getWidth()/2-200, mainClass.getHeight()/2-25, 400, 50);
+		
 		JButton viewPathButton = new JButton("View Path");
-		viewPathButton.setBounds(myFrame.getWidth()/2-200, myFrame.getHeight()/2+40, 400, 50);
+		viewPathButton.setBounds(mainClass.getWidth()/2-200, mainClass.getHeight()/2+40, 400, 50);
 		
 		add(inputNewButton);
 		add(viewPathButton);
